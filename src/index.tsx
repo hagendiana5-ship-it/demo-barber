6fb3f37911eb7d1b9e9d8b30010b35fc4370152d
hagendiana5-ship-it <hagendiana5@gmail.com>
2026-02-06 20:51:51 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import { LanguageProvider } from './context/LanguageContext';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <LanguageProvider>
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
       <App />
-    </LanguageProvider>
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+}
